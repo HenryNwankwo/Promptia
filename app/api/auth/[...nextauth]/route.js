511a34db800b286a651c1a5172e003f6d33d26bb
Nwankwo Henry <nameisifunanya@gmail.com>
2023-10-26 09:02:52 +0100

    Implemented the sign in functionality and updated gitignore

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,5 +1,7 @@
 import NextAuth from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
+import { connectToDB } from '@/utils/db';
+import User from '@/models/user';
 
 const handler = NextAuth({
   providers: [
@@ -11,6 +13,20 @@ const handler = NextAuth({
   async session({ session }) {},
   async signIn({ profile }) {
     try {
+      await connectToDB();
+
+      //check if user exits
+      const userExists = await User.findOne({ email: profile.email });
+
+      //if not, create a new user
+
+      if (!userExists) {
+        await User.create({
+          email: profile.email,
+          username: profile.name.replace(' ', '').toLowerCase(),
+          image: profile.picture,
+        });
+      }
     } catch (error) {}
   },
 });
