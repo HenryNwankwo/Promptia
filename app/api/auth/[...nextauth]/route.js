3c48fb78b6bf2a98798bd28151a0e7b35da0c25b
Nwankwo Henry <nameisifunanya@gmail.com>
2023-10-26 14:53:42 +0100

    Created the form component

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -28,7 +28,7 @@ const handler = NextAuth({
         if (!userExists) {
           await User.create({
             email: profile.email,
-            username: profile.name.replace(' ', '').toLowerCase(),
+            username: profile.email.split('@')[0].toLowerCase(),
             image: profile.picture,
           });
         }
