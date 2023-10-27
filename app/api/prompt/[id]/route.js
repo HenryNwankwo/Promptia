f1c36bd048264535626fae2159cb5e161ec28070
Nwankwo Henry <nameisifunanya@gmail.com>
2023-10-27 14:59:35 +0100

    implemented the UPDATE prompt by id

--- app/api/prompt/[id]/route.js
+++ app/api/prompt/[id]/route.js
@@ -18,5 +18,25 @@ export const GET = async (req, { params }) => {
 };
 
 // PATCH (update)
+export const PATCH = async (req, { params }) => {
+  const { prompt, tag } = await req.json();
+  try {
+    await connectToDB();
+
+    const existingPrompt = await Prompt.findById(params.id);
+    if (!existingPrompt)
+      return new Response('Prompt not found! ', { status: 404 });
+
+    //updating prompt
+    existingPrompt.prompt = prompt;
+    existingPrompt.tag = tag;
+
+    await existingPrompt.save();
+
+    return new Response(JSON.stringify(existingPrompt), { status: 200 });
+  } catch (err) {
+    return new Response('Failed to update prompt! ', { status: 500 });
+  }
+};
 
 // DELETE (delete)
